2961f3ca27340bab2b35225375d19f84f0f3ad10
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-01-16 15:44:21 +0100

    remove watermark

--- app/models/sauna_photo.rb
+++ app/models/sauna_photo.rb
@@ -1,16 +1,17 @@
-require 'paperclip_processors/watermark'
+#require 'paperclip_processors/watermark'
 
 class SaunaPhoto < ActiveRecord::Base
   belongs_to :sauna
   has_attached_file :photo, :processors => [:watermark],
                     :styles => {
                       :thumb => ["100x100", :jpg],
-					  :pagesize => {
-						:geometry => '500x400',
-						:format => :jpg,
-						:watermark_path => "#{Rails.root}/public/images/watermark.png",
-						:position => 'Center'
-					  }						  
+					  :pagesize => ["100x100", :jpg]
+#					  :pagesize => {
+#						:geometry => '500x400',
+#						:format => :jpg,
+#						:watermark_path => "#{Rails.root}/public/images/watermark.png",
+#						:position => 'Center'
+#					  }						  
                     },
                     :default_style => :pagesize
 					
