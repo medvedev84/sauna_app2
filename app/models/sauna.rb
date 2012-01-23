ac9324fc4ed14c42c6df924ab64e930ece772713
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-01-23 08:43:15 +0100

    social

--- app/models/sauna.rb
+++ app/models/sauna.rb
@@ -1,32 +1,36 @@
 class Sauna < ActiveRecord::Base
-  attr_accessible :name, :phone_number1, :phone_number2, :phone_number3
+	attr_accessible :name, :phone_number1, :phone_number2, :phone_number3, :email
+
+	belongs_to :user	
+	has_many :sauna_items, 
+			:dependent => :destroy
+	has_one :address,
+			:dependent => :destroy  
+	has_many :sauna_comments,      
+			:dependent => :destroy		   
+	has_many :sauna_photos,      
+			:dependent => :destroy
+	accepts_nested_attributes_for :sauna_photos, 
+			:allow_destroy => true
   
-  has_many :sauna_photos,      
-           :dependent => :destroy
-		   
-  has_many :sauna_comments,      
-           :dependent => :destroy		   
-  
-  accepts_nested_attributes_for :sauna_photos, :allow_destroy => true
-  
-  validates :name, :presence => true,
+	validates :name, :presence => true,
                    :length   => { :maximum => 50 }
 
-  validates :phone_number1, :presence => true,
+	validates :phone_number1, :presence => true,
                    :length   => { :maximum => 10 }
 
-  has_many :sauna_items, :dependent => :destroy
+	email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  belongs_to :user
-  has_one :address    
-   
-  def sauna_min_price    
-    @sauna_item = sauna_items.min {|a,b| a.min_price <=> b.min_price }    
-    @sauna_item == nil ? 0 : @sauna_item.min_price 
-  end
-  
-  def sauna_max_capacity   
-    @sauna_item = sauna_items.max {|a,b| a.capacity <=> b.capacity }    
-    @sauna_item == nil ? 0 : @sauna_item.capacity 
-  end   
+	validates :email, :allow_blank => true,
+                    :format   => { :with => email_regex }
+					   
+	def sauna_min_price    
+		@sauna_item = sauna_items.min {|a,b| a.min_price <=> b.min_price }    
+		@sauna_item == nil ? 0 : @sauna_item.min_price 
+	end
+
+	def sauna_max_capacity   
+		@sauna_item = sauna_items.max {|a,b| a.capacity <=> b.capacity }    
+		@sauna_item == nil ? 0 : @sauna_item.capacity 
+	end   
 end
