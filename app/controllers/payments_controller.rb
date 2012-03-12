eeb33c4bf396a10e52f7267ccf3c55111c519b6b
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-03-12 20:42:39 +0100

    payment bug fixing

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,26 +2,27 @@ require 'digest/md5'
 
 class PaymentsController < ApplicationController
   def result
-    crc = Payment::get_hash(params['OutSum'], 
-                            params['InvId'], 
-                            Payment::MERCHANT_PASS_2,
-                            "Shp_item=#{params['Shp_item']}")
     @result = "FAIL"
-    1.times do |x|
-      break if params['SignatureValue'].blank? || crc.casecmp(params['SignatureValue']) != 0
+    1.times do |x|      
       @booking = Booking.where(:id => params['Shp_item']).first
-      break if @booking.blank? || @booking.payment.price != params['OutSum'].to_f
-      @booking.payment.booking_id = params['InvId'].to_i
-      @booking.payment.status = Payment::STATUS_OK
-      @booking.payment.save
-      # ...
+      break if @booking.blank? || params['OutSum'].to_f != 1000
+      #create payment
+	  @payment = Payment.new
+	  @payment.price = params['OutSum'].to_f 
+	  @payment.booking_id = @booking.id	
+	  @payment.description = "Payment for booking #" + @booking.id.to_s + " created at " + @booking.created_at.to_s
+      @payment.status = "Payment success"
+      @payment.save
+	  #update booking
+	  @booking.ps_order_id = params['InvId'].to_i
+	  @booking.save		
       @result = "OK#{params['InvId']}"	  	  
     end
 	render 'result', :layout => false	 
   end
 
   def success
-	@booking = Booking.where(:id => params['InvId']).first	
+	@booking = Booking.where(:ps_order_id => params['InvId']).first	
 	if @booking != nil 
 		flash[:success] = :payment_created	
 		@sauna = @booking.sauna	
@@ -33,7 +34,7 @@ class PaymentsController < ApplicationController
   end
 
   def fail
-	@booking = Booking.where(:id => params['InvId']).first
+	@booking = Booking.where(:ps_order_id => params['InvId']).first
 	if @booking != nil 
 		flash[:error] = :payment_not_created
 		@sauna = @booking.sauna	
