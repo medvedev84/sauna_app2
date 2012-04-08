3d7a594237199f557a9bd1d213c89736de1e4d38
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-04-08 13:40:40 +0200

    webmoney

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -7,8 +7,7 @@ class PaymentsController < ApplicationController
 			result_webmoney(params)
 		else
 			result_robokassa(params)
-		end
-		render 'result', :layout => false	 
+		end 
 	end
   
 	def success
@@ -83,47 +82,57 @@ class PaymentsController < ApplicationController
 			end
 			
 			@result = "OK#{params['InvId']}"	
-		end		
+		end	
+		render 'result', :layout => false	 		
 	end
 	
-	def result_webmoney(params)
-		crc = Payment.get_wm_hash(
-			params['LMI_PAYEE_PURSE'], 
-			params['LMI_PAYMENT_AMOUNT'], 
-			params['LMI_PAYMENT_NO'], 
-			params['LMI_MODE'], 
-			params['LMI_SYS_INVS_NO'], 
-			params['LMI_SYS_TRANS_NO'], 
-			params['LMI_SYS_TRANS_DATE'], 
-			params['LMI_SECRET_KEY'], 			
-			params['LMI_PAYER_PURSE'], 	
-			params['LMI_PAYER_WM'])  
+	def result_webmoney(params)		
+		if params["LMI_PAYMENT_AMOUNT"].blank? || params["LMI_PAYEE_PURSE"].blank? || params["LMI_PAYMENT_NO"].blank?
+			render :text => 'FAIL', :status => 500,  :layout => false
+		elsif params['LMI_PREREQUEST'] != nil && params['LMI_PREREQUEST'].eql?('1') 
+				#prerequest
+				if Booking.where(:id => params['LMI_PAYMENT_NO']).first
+					render :text => 'YES', :status => 200,  :layout => false	 	
+				else
+					render :text => "Couldn't find booking #{params['LMI_PAYMENT_NO']}", :status => 500, :layout => false
+				end		
+		else
+			#accept request
+			crc = Payment.get_wm_hash(
+				params['LMI_PAYEE_PURSE'], 
+				params['LMI_PAYMENT_AMOUNT'], 
+				params['LMI_PAYMENT_NO'], 
+				params['LMI_MODE'], 
+				params['LMI_SYS_INVS_NO'], 
+				params['LMI_SYS_TRANS_NO'], 
+				params['LMI_SYS_TRANS_DATE'], 
+				"freedom", 			
+				params['LMI_PAYER_PURSE'], 	
+				params['LMI_PAYER_WM']).upcase   			
 			
-		@result = "FAIL"
-		1.times do |x|      
-			#@result = crc.casecmp(params['LMI_HASH'])
-			#break if params['LMI_HASH'].blank? || crc.casecmp(params['LMI_HASH']) != 0		
-			break if params['LMI_SECRET_KEY'].blank? || params['LMI_SECRET_KEY'] != "freedom"
-			@booking = Booking.where(:id => params['LMI_PAYMENT_NO']).first
-			break if @booking.blank? || params['LMI_PAYMENT_AMOUNT'].to_f != SiteSetting.get_booking_fee
+			@result = "FAIL"			
+			1.times do |x|      				
+				#break if params['LMI_HASH'].blank? || crc.casecmp(params['LMI_HASH']) != 0						
+				@booking = Booking.where(:id => params['LMI_PAYMENT_NO']).first
+				#break if @booking.blank? || params['LMI_PAYMENT_AMOUNT'].to_f != SiteSetting.get_booking_fee
 
-			ActiveRecord::Base.transaction do 
-				#create payment
-				@payment = Payment.new
-				@payment.amount = params['LMI_PAYMENT_AMOUNT'].to_f 
-				@payment.booking_id = @booking.id	
-				@payment.description = t(:payment_created_webmoney) + params['LMI_SYS_INVS_NO'] + ". " + t(:sauna) + ": " + @booking.sauna_item.sauna.name + " (" + @booking.sauna_item.name + ")"
-				@payment.status = Payment.paid
-				@payment.ps_name = "WEBMONEY"
-				@payment.ps_order_id = params['LMI_SYS_INVS_NO']
-				@payment.save
+				ActiveRecord::Base.transaction do 
+					#create payment
+					@payment = Payment.new
+					@payment.amount = params['LMI_PAYMENT_AMOUNT'].to_f 
+					@payment.booking_id = @booking.id	
+					@payment.description = t(:payment_created_webmoney) + params['LMI_SYS_INVS_NO'] + ". " + t(:sauna) + ": " + @booking.sauna_item.sauna.name + " (" + @booking.sauna_item.name + ")"
+					@payment.status = Payment.paid
+					@payment.ps_name = "WEBMONEY"
+					@payment.ps_order_id = params['LMI_SYS_INVS_NO']
+					@payment.save
 
-				#money to application balance
-				Payment.receive_payment(@payment)		  	
+					#money to application balance
+					Payment.receive_payment(@payment)		  	
+				end				
+				@result = "OK"								
 			end
-			
-			@result = "OK#{params['LMI_SYS_INVS_NO']}"			
-				
+			render :text => @result,  :status => 200,  :layout => false	 		
 		end
 	end
 end
