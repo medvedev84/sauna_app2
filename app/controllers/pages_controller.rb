9688663239eb34258e8517379249d53a3b638c3e
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-06-19 20:46:36 +0200

    upgrade mobile and touch version

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,18 +9,6 @@ class PagesController < ApplicationController
   def incorrect
   end
   
-  def all
-	h = params[:q]
-	@q = Sauna.search(h)	
-	if (mobile_device? || touch_device? ) 
-		@saunas = @q.result(:distinct => true)		
-	else	
-		@current_page_number = params[:page] != nil ? params[:page] : 1		
-		@saunas = @q.result(:distinct => true).page(params[:page]).per(10)	
-		respond_to do |format|
-			format.html 
-			format.js
-		end		
-	end		
+  def all	
   end  
 end
