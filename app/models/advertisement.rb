133a95beb890fd478a0345ec30e35ff35b8374dc
Konstantin Medvedev <k.p.medvedev@gmail.com>
2012-06-24 18:16:48 +0200

    image for advertisement

--- app/models/advertisement.rb
+++ app/models/advertisement.rb
@@ -1,3 +1,13 @@
 class Advertisement < ActiveRecord::Base
   belongs_to :city  
+  
+  has_attached_file :photo, 		
+                    :styles => {
+                      :thumb => ["50x50", :jpg],
+					},
+                    :default_style => :thumb,
+					:storage => :s3,
+					:s3_credentials => "#{Rails.root}/config/s3.yml",
+					:path => ":attachment/:id/:style.:extension",
+					:bucket => "go-to-sauna"	  
 end
